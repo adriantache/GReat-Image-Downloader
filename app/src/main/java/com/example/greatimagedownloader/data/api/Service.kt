d208b22bcd5e128c2e8aa70e1930033905edd542
Adrian Tache <contact@adriantache.com>
2023-04-29 12:59:54 +0200

    FINALLY fix OOM issues

--- app/src/main/java/com/example/greatimagedownloader/data/api/Service.kt
+++ app/src/main/java/com/example/greatimagedownloader/data/api/Service.kt
@@ -1,17 +1,12 @@
 package com.example.greatimagedownloader.data.api
 
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 fun getApi(): RicohApi {
     val okHttpClient = OkHttpClient.Builder()
-        .addInterceptor(
-            HttpLoggingInterceptor().apply {
-                level = HttpLoggingInterceptor.Level.BODY
-            }
-        ).build()
+        .build()
 
     val retrofit = Retrofit.Builder()
         .baseUrl("http://192.168.0.1/")
