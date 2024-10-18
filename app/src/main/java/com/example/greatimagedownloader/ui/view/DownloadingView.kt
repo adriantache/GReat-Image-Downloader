d153afed9c6f8baacb3b1d1a36ecd02d9fac91b9
Adrian Tache <contact@adriantache.com>
2024-10-18 20:44:02 +0100

    Improve DownloadingView UI/behaviour

--- app/src/main/java/com/example/greatimagedownloader/ui/view/DownloadingView.kt
+++ app/src/main/java/com/example/greatimagedownloader/ui/view/DownloadingView.kt
@@ -1,31 +1,42 @@
 package com.example.greatimagedownloader.ui.view
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ContextualFlowRow
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.requiredSize
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -35,8 +46,10 @@ import com.example.greatimagedownloader.R
 import com.example.greatimagedownloader.domain.data.model.PhotoDownloadInfo
 import com.example.greatimagedownloader.domain.utils.model.Kbps
 import com.example.greatimagedownloader.ui.util.KeepScreenOn
+import kotlinx.coroutines.delay
 import java.text.DecimalFormat
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun DownloadingView(
     modifier: Modifier = Modifier,
@@ -48,7 +61,7 @@ fun DownloadingView(
     onClose: () -> Unit,
 ) {
     val boxShape = RoundedCornerShape(8.dp)
-    val shimmerBrush = ShimmerBrush()
+    val scrollState = rememberScrollState()
 
     val downloadSpeedDisplay = remember(downloadSpeed) {
         getDownloadSpeed(downloadSpeed.value)
@@ -57,7 +70,17 @@ fun DownloadingView(
         getDownloadSpeedDisplay(downloadSpeed.value)
     }
 
+    var photosSize by remember { mutableIntStateOf(0) }
+
     KeepScreenOn()
+
+    LaunchedEffect(photoDownloadInfo) {
+        if (photosSize != photoDownloadInfo.size) {
+            photosSize = photoDownloadInfo.size
+            delay(1000) // required to let the bitmap inflate
+            scrollState.animateScrollTo(scrollState.maxValue)
+        }
+    }
 
     Column(
         modifier = modifier
@@ -66,6 +89,26 @@ fun DownloadingView(
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         CloseIcon(onClose)
+
+        AnimatedVisibility(isStopping) {
+            Column {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(MaterialTheme.colorScheme.errorContainer)
+                        .padding(16.dp),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    Text(
+                        text = "Stopping download...",
+                        style = MaterialTheme.typography.titleMedium,
+                        color = MaterialTheme.colorScheme.onErrorContainer,
+                    )
+                }
+
+                Spacer(Modifier.height(16.dp))
+            }
+        }
 
         Box(
             modifier = Modifier
@@ -88,71 +131,71 @@ fun DownloadingView(
 
         Spacer(Modifier.height(16.dp))
 
-        LinearProgressIndicator(
-            progress = currentPhoto.toFloat() / totalPhotos
-        )
+        LinearProgressIndicator(progress = { currentPhoto.toFloat() / totalPhotos })
 
         Spacer(Modifier.height(16.dp))
 
-        LazyVerticalGrid(
-            columns = GridCells.Adaptive(minSize = 100.dp),
+        val density = LocalDensity.current
+        var totalFlowWidth by remember { mutableStateOf(0.dp) }
+
+        ContextualFlowRow(
+            modifier = Modifier
+                .safeDrawingPadding()
+                .fillMaxWidth(1f)
+                .onGloballyPositioned { totalFlowWidth = with(density) { it.size.width.toDp() } }
+                .wrapContentHeight(align = Alignment.CenterVertically)
+                .verticalScroll(scrollState),
             verticalArrangement = Arrangement.spacedBy(4.dp),
-            horizontalArrangement = Arrangement.spacedBy(4.dp),
-        ) {
-            items(items = photoDownloadInfo) { mediaInfo ->
-                Box(
-                    modifier = Modifier
-                        .aspectRatio(1f)
-                        .background(MaterialTheme.colorScheme.primary, boxShape),
-                    contentAlignment = Alignment.Center,
-                ) {
-                    when {
-                        !mediaInfo.isFinished -> Text(
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            itemCount = photoDownloadInfo.size,
+            maxItemsInEachRow = 3,
+        ) { index ->
+            val mediaInfo = photoDownloadInfo[index]
+            val width = (totalFlowWidth - 16.dp) / 3
+
+            Box(
+                modifier = Modifier
+                    .requiredWidth(width)
+                    .background(MaterialTheme.colorScheme.primary, boxShape),
+                contentAlignment = Alignment.Center,
+            ) {
+                when {
+                    !mediaInfo.isFinished -> Box(
+                        modifier = Modifier
+                            .requiredWidth(width)
+                            .requiredHeight(width / 2),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        Text(
                             text = "${mediaInfo.downloadProgress}%",
                             color = MaterialTheme.colorScheme.onPrimary,
                         )
-
-                        mediaInfo.isImage -> AsyncImage(
-                            model = mediaInfo.uri,
-                            modifier = Modifier.clip(boxShape),
-                            contentDescription = null,
-                            contentScale = ContentScale.Crop,
-                            placeholder = painterResource(R.drawable.baseline_landscape_24),
-                        )
-
-                        else -> Box(
-                            modifier = Modifier
-                                .aspectRatio(1f)
-                                .background(MaterialTheme.colorScheme.primary, boxShape),
-                            contentAlignment = Alignment.Center,
-                        ) {
-                            Icon(
-                                painter = painterResource(id = R.drawable.movie),
-                                contentDescription = null,
-                                modifier = Modifier.requiredSize(28.dp),
-                                tint = MaterialTheme.colorScheme.onPrimary,
-                            )
-                        }
                     }
-                }
-            }
 
-            if (!isStopping) {
-                items(items = (0 until totalPhotos - photoDownloadInfo.size).toList()) {
-                    ProgressShimmer(shimmerBrush, boxShape)
+                    mediaInfo.isImage -> AsyncImage(
+                        model = mediaInfo.uri,
+                        modifier = Modifier.clip(boxShape),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        placeholder = painterResource(R.drawable.baseline_landscape_24),
+                    )
+
+                    else -> Box(
+                        modifier = Modifier
+                            .background(MaterialTheme.colorScheme.primary, boxShape),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.movie),
+                            contentDescription = null,
+                            modifier = Modifier.requiredSize(28.dp),
+                            tint = MaterialTheme.colorScheme.onPrimary,
+                        )
+                    }
                 }
             }
         }
     }
-}
-
-@Composable
-private fun ProgressShimmer(shimmerBrush: Brush, boxShape: RoundedCornerShape) {
-    Box(
-        modifier = Modifier
-            .aspectRatio(1f)
-            .background(shimmerBrush, boxShape)
-    )
 }
 
 private fun getDownloadSpeed(downloadSpeedKb: Double): String {
