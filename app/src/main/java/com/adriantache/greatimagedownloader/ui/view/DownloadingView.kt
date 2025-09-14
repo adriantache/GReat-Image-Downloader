fa5c9f58a8a5641896fce5f1a7d85dd336e2bb3d
Adrian Tache <contact@adriantache.com>
2025-09-14 21:29:36 +0100

    Replace deprecated component with new view (WIP)

--- app/src/main/java/com/adriantache/greatimagedownloader/ui/view/DownloadingView.kt
+++ app/src/main/java/com/adriantache/greatimagedownloader/ui/view/DownloadingView.kt
@@ -1,25 +1,16 @@
 package com.adriantache.greatimagedownloader.ui.view
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ContextualFlowRow
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredHeight
-import androidx.compose.foundation.layout.requiredSize
-import androidx.compose.foundation.layout.requiredWidth
-import androidx.compose.foundation.layout.safeDrawingPadding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -27,22 +18,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import com.adriantache.greatimagedownloader.R
 import com.adriantache.greatimagedownloader.domain.data.model.PhotoDownloadInfo
 import com.adriantache.greatimagedownloader.domain.utils.model.Kbps
 import kotlinx.coroutines.delay
@@ -113,67 +96,10 @@ fun DownloadingView(
 
         Spacer(Modifier.height(16.dp))
 
-        val density = LocalDensity.current
-        var totalFlowWidth by remember { mutableStateOf(0.dp) }
-
-        // TODO: find a new solution to this mess
-        ContextualFlowRow(
-            modifier = Modifier
-                .safeDrawingPadding()
-                .fillMaxWidth(1f)
-                .onGloballyPositioned { totalFlowWidth = with(density) { it.size.width.toDp() } }
-                .wrapContentHeight(align = Alignment.CenterVertically)
-                .verticalScroll(scrollState),
-            verticalArrangement = Arrangement.spacedBy(4.dp),
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            itemCount = photoDownloadInfo.size,
-            maxItemsInEachRow = 3,
-        ) { index ->
-            val mediaInfo = photoDownloadInfo[index]
-            val width = (totalFlowWidth - 16.dp) / 3 * if (mediaInfo.isLandscape(context)) 2 else 1
-
-            Box(
-                modifier = Modifier
-                    .requiredWidth(width)
-                    .background(MaterialTheme.colorScheme.primary, boxShape),
-                contentAlignment = Alignment.Center,
-            ) {
-                when {
-                    !mediaInfo.isFinished -> Box(
-                        modifier = Modifier
-                            .requiredWidth(width)
-                            .requiredHeight(width / 2),
-                        contentAlignment = Alignment.Center,
-                    ) {
-                        Text(
-                            text = "${mediaInfo.downloadProgress}%",
-                            color = MaterialTheme.colorScheme.onPrimary,
-                        )
-                    }
-
-                    mediaInfo.isImage -> AsyncImage(
-                        model = mediaInfo.uri,
-                        modifier = Modifier.clip(boxShape),
-                        contentDescription = null,
-                        contentScale = ContentScale.Crop,
-                        placeholder = painterResource(R.drawable.baseline_landscape_24),
-                    )
-
-                    else -> Box(
-                        modifier = Modifier
-                            .background(MaterialTheme.colorScheme.primary, boxShape),
-                        contentAlignment = Alignment.Center,
-                    ) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.movie),
-                            contentDescription = null,
-                            modifier = Modifier.requiredSize(28.dp),
-                            tint = MaterialTheme.colorScheme.onPrimary,
-                        )
-                    }
-                }
-            }
-        }
+        SmartFlowRow(
+            modifier = Modifier.fillMaxWidth(),
+            photoDownloadInfo = photoDownloadInfo,
+        )
     }
 }
 
--- app/src/main/java/com/adriantache/greatimagedownloader/ui/view/SmartFlowRow.kt
+++ app/src/main/java/com/adriantache/greatimagedownloader/ui/view/SmartFlowRow.kt
@@ -1,53 +1,105 @@
 package com.adriantache.greatimagedownloader.ui.view
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.animateScrollBy
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.adriantache.greatimagedownloader.R
 import com.adriantache.greatimagedownloader.domain.data.model.PhotoDownloadInfo
 
 @Composable
-fun SmartFlowRow(modifier: Modifier = Modifier) {
+fun SmartFlowRow(
+    modifier: Modifier = Modifier,
+    photoDownloadInfo: List<PhotoDownloadInfo>,
+) {
+    val density = LocalDensity.current
+    val state = rememberLazyGridState()
 
+    var totalFlowWidth by remember { mutableStateOf(0.dp) }
+    var latestRememberedFinishedImage by remember { mutableIntStateOf(0) }
+    val latestFinishedImage = remember(photoDownloadInfo) {
+        derivedStateOf { photoDownloadInfo.indexOfLast { it.isFinished } }
+    }.value
+
+    LaunchedEffect(latestFinishedImage) {
+        if (latestRememberedFinishedImage != latestFinishedImage) {
+            state.animateScrollBy(9999f)
+            latestRememberedFinishedImage = latestFinishedImage
+        }
+    }
+
+    LazyVerticalGrid(
+        modifier = modifier.onGloballyPositioned {
+            if (totalFlowWidth != 0.dp) return@onGloballyPositioned
+            totalFlowWidth = with(density) { it.size.width.toDp() - 8.dp - 4.dp }
+        },
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        columns = GridCells.Adaptive((totalFlowWidth / 2).coerceAtLeast(2.dp)),
+        state = state,
+    ) {
+        items(photoDownloadInfo) { info ->
+            SmartFlowRowItem(info, totalFlowWidth = totalFlowWidth)
+        }
+    }
 }
 
 @Composable
-fun RowScope.SmartFlowRowItem(
+fun SmartFlowRowItem(
     mediaInfo: PhotoDownloadInfo,
+    totalFlowWidth: Dp,
 ) {
     val context = LocalContext.current
+    val boxShape = RoundedCornerShape(8.dp)
 
-    val weight = if (mediaInfo.isLandscape(context)) 1f else 0.5f
+    val widthMultiplier = when {
+        !mediaInfo.isFinished -> 0.5f
+        !mediaInfo.isImage -> 0.5f
+        else -> if (mediaInfo.isLandscape(context)) 1f else 0.5f
+    }
+    val requiredWidth = totalFlowWidth * widthMultiplier
 
     Box(
         modifier = Modifier
-            .weight(weight)
+            .requiredWidth(requiredWidth)
             .background(MaterialTheme.colorScheme.primary, boxShape),
         contentAlignment = Alignment.Center,
     ) {
         when {
-            !mediaInfo.isFinished -> Box(
-                modifier = Modifier
-                    .requiredWidth(width)
-                    .requiredHeight(width / 2),
-                contentAlignment = Alignment.Center,
-            ) {
+            !mediaInfo.isFinished -> Box(contentAlignment = Alignment.Center) {
                 Text(
+                    modifier = Modifier.height(24.dp),
                     text = "${mediaInfo.downloadProgress}%",
                     color = MaterialTheme.colorScheme.onPrimary,
                 )
@@ -55,15 +107,16 @@ fun RowScope.SmartFlowRowItem(
 
             mediaInfo.isImage -> AsyncImage(
                 model = mediaInfo.uri,
-                modifier = Modifier.clip(boxShape),
+                modifier = Modifier
+                    .requiredWidth(requiredWidth)
+                    .clip(boxShape),
                 contentDescription = null,
-                contentScale = ContentScale.Crop,
+                contentScale = ContentScale.FillWidth,
                 placeholder = painterResource(R.drawable.baseline_landscape_24),
             )
 
             else -> Box(
-                modifier = Modifier
-                    .background(MaterialTheme.colorScheme.primary, boxShape),
+                modifier = Modifier.background(MaterialTheme.colorScheme.primary, boxShape),
                 contentAlignment = Alignment.Center,
             ) {
                 Icon(
