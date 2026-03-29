b939fa5a91f616d958656f1c095c31f39418a68b
Adrian Tache <contact@adriantache.com>
2026-03-29 21:16:40 +0100

    Update SmartFlowRow to use GridItemSpan

--- app/src/main/java/com/adriantache/greatimagedownloader/ui/view/SmartFlowRow.kt
+++ app/src/main/java/com/adriantache/greatimagedownloader/ui/view/SmartFlowRow.kt
@@ -4,10 +4,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.animateScrollBy
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.requiredSize
-import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
@@ -20,19 +21,13 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.adriantache.greatimagedownloader.R
@@ -43,10 +38,8 @@ fun SmartFlowRow(
     modifier: Modifier = Modifier,
     photoDownloadInfo: List<PhotoDownloadInfo>,
 ) {
-    val density = LocalDensity.current
     val state = rememberLazyGridState()
 
-    var totalFlowWidth by remember { mutableStateOf(0.dp) }
     var latestRememberedFinishedImage by remember { mutableIntStateOf(0) }
     val latestFinishedImage = remember(photoDownloadInfo) {
         derivedStateOf { photoDownloadInfo.indexOfLast { it.isFinished } }
@@ -60,17 +53,21 @@ fun SmartFlowRow(
     }
 
     LazyVerticalGrid(
-        modifier = modifier.onGloballyPositioned {
-            if (totalFlowWidth != 0.dp) return@onGloballyPositioned
-            totalFlowWidth = with(density) { it.size.width.toDp() - 8.dp - 4.dp }
-        },
+        modifier = modifier,
+        columns = GridCells.Fixed(2),
         verticalArrangement = Arrangement.spacedBy(4.dp),
         horizontalArrangement = Arrangement.spacedBy(8.dp),
-        columns = GridCells.Adaptive((totalFlowWidth / 2).coerceAtLeast(2.dp)),
         state = state,
     ) {
-        items(photoDownloadInfo) { info ->
-            SmartFlowRowItem(info, totalFlowWidth = totalFlowWidth)
+        items(
+            items = photoDownloadInfo,
+            key = { it.uri },
+            span = { info ->
+                val isLandscape = info.isFinished && info.isImage && info.isLandscape
+                GridItemSpan(if (isLandscape) 2 else 1)
+            }
+        ) { info ->
+            SmartFlowRowItem(info)
         }
     }
 }
@@ -78,21 +75,12 @@ fun SmartFlowRow(
 @Composable
 fun SmartFlowRowItem(
     mediaInfo: PhotoDownloadInfo,
-    totalFlowWidth: Dp,
 ) {
-    val context = LocalContext.current
     val boxShape = RoundedCornerShape(8.dp)
-
-    val widthMultiplier = when {
-        !mediaInfo.isFinished -> 0.5f
-        !mediaInfo.isImage -> 0.5f
-        else -> if (mediaInfo.isLandscape(context)) 1f else 0.5f
-    }
-    val requiredWidth = totalFlowWidth * widthMultiplier
 
     Box(
         modifier = Modifier
-            .requiredWidth(requiredWidth)
+            .fillMaxWidth()
             .background(MaterialTheme.colorScheme.primary, boxShape),
         contentAlignment = Alignment.Center,
     ) {
@@ -108,7 +96,7 @@ fun SmartFlowRowItem(
             mediaInfo.isImage -> AsyncImage(
                 model = mediaInfo.uri,
                 modifier = Modifier
-                    .requiredWidth(requiredWidth)
+                    .fillMaxWidth()
                     .clip(boxShape),
                 contentDescription = null,
                 contentScale = ContentScale.FillWidth,
